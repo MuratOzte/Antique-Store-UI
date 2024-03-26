58633e7af2272341dd2dc48c1a5007406ba28191
MuratOzte <mo2553481@gmail.com>
2024-03-26 21:54:48 +0300

    Reorder components and update styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import Nav from './Components/nav/Nav';
-import MobileLinks from './Components/nav/MobileLinks';
-import Hero from './Components/hero/Hero';
-import Selections from './Components/Selections/Selections';
-import RecentlyAdded from './Components/RecentlyAdded/RecentlyAdded';
-import TopCategories from './Components/topCategories/TopCategories';
 import FeedBack from './Components/feedback/Feedback';
-import Subscribe from './Components/subscribe/Subscribe';
 import Footer from './Components/footer/Footer';
+import Hero from './Components/hero/Hero';
+import MobileLinks from './Components/nav/MobileLinks';
+import Nav from './Components/nav/Nav';
+import RecentlyAdded from './Components/RecentlyAdded/RecentlyAdded';
+import Selections from './Components/Selections/Selections';
+import Subscribe from './Components/subscribe/Subscribe';
+import TopCategories from './Components/topCategories/TopCategories';
 
+import { AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+
+import './App.scss';
 
 function App() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <div style={containerStyle}>
-            <div style={containerItem}>
+        <div className="container">
+            <div className="item">
                 <Nav setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />
                 <AnimatePresence>
                     {isMenuOpen && <MobileLinks />}
@@ -24,20 +26,20 @@ function App() {
                 <Hero />
             </div>
             <div
+                className="item"
                 style={{
-                    ...containerItem,
                     paddingTop: '2px',
                 }}
             >
                 <Selections />
                 <RecentlyAdded />
             </div>
-            <div style={containerItem}>
+            <div className="item">
                 <TopCategories />
             </div>
             <div
+                className="item"
                 style={{
-                    ...containerItem,
                     display: 'flex',
                     justifyContent: 'center',
                 }}
@@ -45,8 +47,8 @@ function App() {
                 <FeedBack />
             </div>
             <div
+                className="item"
                 style={{
-                    ...containerItem,
                     paddingTop: '16px',
                 }}
             >
@@ -56,19 +58,5 @@ function App() {
         </div>
     );
 }
-
-const containerStyle = {
-    scrollSnapType: 'y mandatory',
-    width: '100%',
-    height: '100vh',
-    overflowY: 'scroll',
-};
-
-const containerItem = {
-    scrollSnapAlign: 'center',
-    scrollSnapStop: 'always',
-    height: '100vh',
-    width: '100%',
-};
 
 export default App;
